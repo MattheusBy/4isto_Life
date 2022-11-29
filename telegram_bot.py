658ee6706c914d3188e13c414ae3f8070e5dfd89
MattheusBy <mattaudia6@gmail.com>
2022-11-29 21:17:19 +0300

    Create Dockerfile Change Items images

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,4 +1,3 @@
-import requests
 import telegram
 from flask import request
 
@@ -9,14 +8,14 @@ def send_to_telegram():
     CHAT_ID = '-815155782'
     TOKEN = "5661560876:AAERwxrmUbxPBmvD54-Ootboe_cbclyva1s"
     message = "От {0}\n Телефон {1}. \n Населенный пункт: {2}. \n Изделие: {3} в количестве {4}.\n Запах мочи: {5} \n Сушка: {6} \n Чистка подушек: {7}".format(
-                data['name'],
-                data['phone'],
-                data['city'],
-                data['item'],
-                data['count'],
-                data['urine'],
-                data['drying'],
-                data['pillows'],)
+        data['name'],
+        data['phone'],
+        data['city'],
+        data['item'],
+        data['count'],
+        data['urine'],
+        data['drying'],
+        data['pillows'], )
     bot = telegram.Bot(token=TOKEN)
     bot.send_photo(chat_id=CHAT_ID, photo=open('uploaded_image/imageToSave.jpg', 'rb'))
     bot.send_message(chat_id=CHAT_ID, text=message)
