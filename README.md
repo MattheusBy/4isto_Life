787e27910dddf22fd490143df52d05618bb5be6d
MattheusBy <mattaudia6@gmail.com>
2022-12-19 10:32:07 +0300

    first commit to new_branch

---
+++ README.md
@@ -0,0 +1,48 @@
+--------------------------------------------------------------------------4istoLIFE --------------------------------------------------------------------
+
+11/12/2022
+
+Author: Matsvei Malchanau
+
+Email: matsvey.malchanau.python@gmail.com
+
+LinkedIn: www.linkedin.com/in/matsvey-malchanau
+
+Github: github.com/MattheusBy
+
+--------------------------------------------------------------------------Description------------------------------------------------------------------
+
+The purpose of this project is to study and apply technologies and simulate a real commercial project. 
+I have experience in dry cleaning of upholstered furniture and understand the tasks and functionality that are needed for such a site. I tried to realize
+this things in my project.
+
+Main aims:
+- The most realistic project
+- Only necessary functionality
+- Beautiful design without unnecessary details
+- Completely finished and tested project
+
+You can find a description of the company, examples of work on the index-page. The Process tab describes the product dry cleaning process. 
+Price for each type of furniture is in the "prices" section. Through the form on the "get_price" tab you can send data for an accurate estimate 
+of the cost of cleaning. The data will send to the telegram-bot and the manager will send of cleaning upholstery. At the "calendar"-page, 
+you can find the cleaning schedule and choose the appropriate date and time for you. 
+On the tab "contacts" you can find instant messengers for communication.
+
+--------------------------------------------------------------------------Technologies----------------------------------------------------------------
+
+- Flask (base)
+- Poetry (virtual enviroment)
+- Telebot (Telegram-bot for send price request)
+- Pytest
+- Coverage
+- Docker/Docker-compose
+- Waitress
+- Nginx
+- Bandit (check security)
+- Bootstarap 5
+- Flake8
+
+---------------------------------------------------------------------------Gratitude-------------------------------------------------------------------
+
+- Bootstrapmade (https://bootstrapmade.com/)
+- Svgwave (https://svgwave.in/)
---
+++ web/main_app/__init__.py
@@ -0,0 +1,78 @@
+"""
+This package creates Flask-application and sets configuration.
+It contains view-fuctions for renders templates ant its url-routes
+"""
+
+import os
+from flask import Flask, render_template, request
+from main_app.telegram_bot import send_to_telegram
+
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route('/')
+    def index():
+        # view-function for index-page
+        return render_template('index.html')
+
+    @app.route('/process')
+    def process():
+        # views-function for process-page
+        return render_template('process.html')
+
+    @app.route('/prices')
+    def prices():
+        # views-function for prices-page
+        return render_template('prices.html')
+
+    @app.route('/get_price', methods=['post', 'get'])
+    def get_price():
+        # views-function for get_price-page
+        if request.method == 'POST':
+            send_to_telegram()
+            return render_template('request_send.html')
+        if request.method == 'GET':
+            return render_template('get_price.html')
+
+    @app.route('/contacts')
+    def contacts():
+        # views-function for contacts_page
+        return render_template('contacts.html')
+
+    @app.route('/calendar')
+    def calendar():
+        # views-function for calendar-page
+        return render_template("calendar.html")
+
+    @app.route('/order')
+    def order():
+        # views-function for order-page
+        return render_template("order.html")
+
+    @app.errorhandler(404)
+    def invalid_route(e):
+        return render_template("404.html")
+
+    @app.errorhandler(500)
+    def invalid_route(e):
+        return render_template("500.html")
+
+    return app
