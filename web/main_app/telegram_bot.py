ca9e6c0c242718a2a65bdf8e41fcf7ef940f2363
MattheusBy <mattaudia6@gmail.com>
2022-12-19 11:29:39 +0300

    change telegram_bot methods for send message

--- web/main_app/telegram_bot.py
+++ web/main_app/telegram_bot.py
@@ -3,8 +3,7 @@
    Owner of app can get this information in his telegram-account associated with telegram-bot and can
    send back actual price to user use SMS.
 """
-
-import telebot
+import requests
 from flask import request
 
 
@@ -13,10 +12,10 @@ def send_to_telegram():
     data = request.form.to_dict()
     request.files["photo"].save("main_app/uploaded_image/imageToSave.jpeg")
     # CHAT_ID and TOKEN provides permissions to send data to owner's app
-    CHAT_ID = '-815155782'
+    CHAT_ID = '-1001599179085'
     TOKEN = "5661560876:AAERwxrmUbxPBmvD54-Ootboe_cbclyva1s"
     # Create message-body
-    message = "От {0}\n Телефон {1}. \n Населенный пункт: {2}. \n Изделие: {3} в количестве {4}.\n Запах мочи: {5} \n Сушка: {6} \n Чистка подушек: {7}".format(
+    message = "От:{0}\n Телефон {1}. \n Населенный пункт: {2}. \n Изделие: {3} в количестве {4}.\n Запах мочи: {5} \n Сушка: {6} \n Чистка подушек: {7}".format(
         data['name'],
         data['phone'],
         data['city'],
@@ -25,7 +24,16 @@ def send_to_telegram():
         data['urine'],
         data['drying'],
         data['pillows'],)
-    # Create Telebot instance and send user's data
-    bot = telebot.TeleBot(token=TOKEN)
-    bot.send_photo(chat_id=CHAT_ID, photo=open("main_app/uploaded_image/imageToSave.jpeg", 'rb'))
-    bot.send_message(chat_id=CHAT_ID, text=message)
+    url_text = "https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={message}".format(token=TOKEN, chat_id=CHAT_ID, message=message)
+    url_photo = "https://api.telegram.org/bot{token}/sendPhoto?chat_id={chat_id}".format(token=TOKEN, chat_id=CHAT_ID)
+    files = [
+        ('photo',
+         ('imageToSave.jpeg', open('main_app/uploaded_image/imageToSave.jpeg', 'rb'), 'image/jpeg'))
+    ]
+    payload = {}
+    headers = {}
+
+    response_photo = requests.request("POST", url_photo, headers=headers, files=files)
+    response_text = requests.request("POST", url_text, headers=headers)
+    if response_text and response_photo:
+        return "Message has send successful!!"
--- web/tests/test_views_functions.py
+++ web/tests/test_views_functions.py
@@ -13,11 +13,10 @@ def test_config():
     assert create_app({'TESTING': True}).testing
 
 
-
 def test_index(client):
     # sends request to index-page and checks response
     response = client.get('/')
-    assert response.status_code==200
+    assert response.status_code == 200
     assert b'get_price' in response.data
     assert b'src="static/img/works/item-6.jpg"' in response.data
     assert b'All rights reserved' in response.data
@@ -29,8 +28,8 @@ def test_index(client):
 def test_calendar(client):
     # sends request to calendar-page and checks response
     response = client.get('/calendar')
-    assert response.status_code==200
-    assert bytes('Расписание чисток', encoding = 'utf-8') in response.data
+    assert response.status_code == 200
+    assert bytes('Расписание чисток', encoding='utf-8') in response.data
     assert b'<iframe src="https://calendar.google.com/calendar/' in response.data
 
 
@@ -41,10 +40,12 @@ def test_contacts(client):
     assert bytes('Наши контакты', encoding='utf-8') in response.data
     assert b'https://www.instagram.com/4istolife_gomel/' in response.data
     assert b'https://ru-ru.facebook.com/' in response.data
+
+
 def test_prices(client):
     # sends request to prices-page and checks response
     response = client.get('/prices')
-    assert response.status_code==200
+    assert response.status_code == 200
     assert b'src="static/img/sofas/divan-1.png"' in response.data
     assert b'35 BYN' in response.data
     assert bytes('Удаление запаха', encoding='utf-8') in response.data
@@ -54,52 +55,53 @@ def test_prices(client):
 def test_process(client):
     # sends request to process-page and checks response
     response = client.get('/process')
-    assert response.status_code==200
+    assert response.status_code == 200
     assert b'src="static/img/process/process-0.jpg"' in response.data
     assert b'src="static/img/process/process-2.jpg"' in response.data
-    assert bytes('Нейтрализация', encoding = 'utf-8') in response.data
+    assert bytes('Нейтрализация', encoding='utf-8') in response.data
 
 
 def test_order(client):
     # sends request to order-page and checks response
     response = client.get('/order')
-    assert response.status_code==200
+    assert response.status_code == 200
     assert b'src="static/img/order/order-1.png"' in response.data
     assert b'src="static/img/order/order-2.png"' in response.data
-    assert bytes('Как заказать', encoding = 'utf-8') in response.data
-    assert bytes('Для осуществления заказа', encoding = 'utf-8') in response.data
-    assert bytes('Сделайте фото Вашего изделия в хорошем качестве', encoding = 'utf-8') in response.data
+    assert bytes('Как заказать', encoding='utf-8') in response.data
+    assert bytes('Для осуществления заказа', encoding='utf-8') in response.data
+    assert bytes('Сделайте фото Вашего изделия в хорошем качестве', encoding='utf-8') in response.data
+
 
 def test_get_price(client):
     # sends request to get_price-page and checks response
     response = client.get('/get_price')
-    assert response.status_code==200
+    assert response.status_code == 200
     # send POST-request to localhost
     url = "http://127.0.0.1:5000/get_price"
     # dict with test data in POST-request
     payload_1 = {'name': 'Alex',
-               'phone': '+375291111111',
-               'city': 'Gomel',
-               'item': 'Трехместный диван',
-               'count': '1',
-               'urine': 'нет',
-               'drying': 'нет',
-               'pillows': 'да'}
+                 'phone': '+375291111111',
+                 'city': 'Gomel',
+                 'item': 'Трехместный диван',
+                 'count': '1',
+                 'urine': 'нет',
+                 'drying': 'нет',
+                 'pillows': 'да'}
     # add image in POST-request
     files = [
         ('photo', ('img_for_test.jpg', open('web/tests/img_for_test.jpg', 'rb'), 'image/jpeg'))
     ]
     response = requests.request("POST", url, data=payload_1, files=files)
-    assert response.status_code==200
+    assert response.status_code == 200
     # dict with test data in POST-request
     payload_2 = {'name': 'Jim',
-               'phone': '+3752912345678',
-               'city': 'Gomel',
-               'item': 'Кресло',
-               'count': '4',
-               'urine': 'нет',
-               'drying': 'нет',
-               'pillows': 'нет'}
+                 'phone': '+3752912345678',
+                 'city': 'Gomel',
+                 'item': 'Кресло',
+                 'count': '4',
+                 'urine': 'нет',
+                 'drying': 'нет',
+                 'pillows': 'нет'}
     # add image in POST-request
     files = [
         ('photo', ('img_for_test.jpg', open('web/tests/img_for_test.jpg', 'rb'), 'image/jpeg'))
@@ -109,15 +111,15 @@ def test_get_price(client):
     assert bytes('Ожидайте SMS', encoding='utf-8') in response.content
     assert bytes('15 минут', encoding='utf-8') in response.content
 
+
 def test_request_send(client):
     # sends request to request_send_page and checks response. Direct request response is error 404
     response = client.get('/request_send')
-    assert response.status_code==200
+    assert response.status_code == 200
     assert b"url('static/img/errors_images/404_back.png')" in response.data
 
 
 def test_error_handler_404(client):
     response = client.get('/some_text')
-    assert response.status_code==200
+    assert response.status_code == 200
     assert b"url('static/img/errors_images/404_back.png')" in response.data
-
