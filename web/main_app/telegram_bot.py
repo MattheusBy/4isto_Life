bc06927f9223ba0d1d0751daad83b7f3477895d5
MattheusBy <mattaudia6@gmail.com>
2022-12-25 15:41:16 +0300

    add selenium for test create test_interface.py change index.html (added name for "Our works" section) update poetry dependencies

--- web/main_app/telegram_bot.py
+++ web/main_app/telegram_bot.py
@@ -1,6 +1,8 @@
 """
-   Module retrieves data from url-page "get_price". After then, it sends data from user to telegram-bot
-   Owner of app can get this information in his telegram-account associated with telegram-bot and can
+   Module retrieves data from url-page "get_price".
+   After then, it sends data from user to telegram-bot
+   Owner of app can get this information
+   in his telegram-account associated with telegram-bot and can
    send back actual price to user use SMS.
 """
 import requests
@@ -15,17 +17,24 @@ def send_to_telegram():
     CHAT_ID = '-1001599179085'
     TOKEN = "5661560876:AAERwxrmUbxPBmvD54-Ootboe_cbclyva1s"
     # Create message-body
-    message = "От:{0}\n Телефон {1}. \n Населенный пункт: {2}. \n Изделие: {3} в количестве {4}.\n Запах мочи: {5} \n Сушка: {6} \n Чистка подушек: {7}".format(
-        data['name'],
-        data['phone'],
-        data['city'],
-        data['item'],
-        data['count'],
-        data['urine'],
-        data['drying'],
-        data['pillows'],)
-    url_text = "https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={message}".format(token=TOKEN, chat_id=CHAT_ID, message=message)
-    url_photo = "https://api.telegram.org/bot{token}/sendPhoto?chat_id={chat_id}".format(token=TOKEN, chat_id=CHAT_ID)
+    message = "От:{0}\n Телефон {1}. \n " \
+              "Населенный пункт: {2}. \n " \
+              "Изделие: {3} в количестве {4}.\n " \
+              "Запах мочи: {5} " \
+              "\n Сушка: {6} " \
+              "\n Чистка подушек: {7}".format(
+                data['name'],
+                data['phone'],
+                data['city'],
+                data['item'],
+                data['count'],
+                data['urine'],
+                data['drying'],
+                data['pillows'], )
+    url_text = "https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={message}".format(
+        token=TOKEN, chat_id=CHAT_ID, message=message)
+    url_photo = "https://api.telegram.org/bot{token}/sendPhoto?chat_id={chat_id}".format(
+        token=TOKEN, chat_id=CHAT_ID)
     files = [
         ('photo',
          ('imageToSave.jpeg', open('main_app/uploaded_image/imageToSave.jpeg', 'rb'), 'image/jpeg'))
@@ -36,4 +45,4 @@ def send_to_telegram():
     response_photo = requests.request("POST", url_photo, headers=headers, files=files)
     response_text = requests.request("POST", url_text, headers=headers)
     if response_text and response_photo:
-        return "Message has send successful!!"
+        return "Message has been send successful!!"
